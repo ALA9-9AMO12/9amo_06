c150547f4e22e5b2fa128373dc640ca0eee6254e
justgogamer <6004335@mborijnland.nl>
2021-06-08 10:26:21 +0200

    Update WebsiteController.php

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Post;
-use App\Category;
+use App\posts;
+use App\courses;
 
 class WebsiteController extends Controller
 {
     public function index(){
-        $categories = Category::orderBy('name', 'ASC')->where('is_published', 'l')->get();
-        $posts = Post::orderBy('id', 'DESC')->where('post_type', 'post')->where('is_published', 'l')->paginate(5);
-        return view('website.index', compact('posts', 'categories'));
+        $courses = courses::orderBy('title', 'ASC')->get();
+        $posts = posts::orderBy('date', 'DESC')->paginate(5);
+        return view('website.index', compact('posts', 'courses'));
     }
 }
